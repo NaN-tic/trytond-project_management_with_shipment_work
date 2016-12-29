a01da3b5b67855804b7d7e5b168de31cb23ccc39
?ngel ?lvarez <angel@nan-tic.com>
2016-12-29 17:34:47 +0100

    fixes

--- work.py
+++ work.py
@@ -12,7 +12,7 @@ class ShipmentWork:
     __name__ = 'shipment.work'
     __metaclass__ = PoolMeta
 
-    parent = fields.Many2One('project.work', 'Project')
+    project = fields.Many2One('project.work', 'Project')
 
     @classmethod
     def pending(cls, shipments):
@@ -22,17 +22,19 @@ class ShipmentWork:
         cls.save(shipments)
         super(ShipmentWork, cls).pending(shipments)
 
-
     @classmethod
     def _get_cost(cls, shipments):
         vals = cls.get_cost(shipments, 'cost')
         return vals['cost']
-        
+
     @classmethod
     def _get_revenue(cls, shipments):
         vals = cls.get_cost(shipments, 'revenue')
         return vals['revenue']
 
+    @staticmethod
+    def _get_summary_related_field():
+        return 'project'
 
 
 class Work:
@@ -40,11 +42,10 @@ class Work:
     __name__ = 'project.work'
     __metaclass__ = PoolMeta
 
-
     @classmethod
     def _get_related_cost_and_revenue(cls):
         res = super(Work, cls)._get_related_cost_and_revenue()
-        return res + [('shipment.work', 'parent', '_get_revenue',
+        return res + [('shipment.work', 'project', '_get_revenue',
             '_get_cost')]
 
 
@@ -52,6 +53,7 @@ class ProjectSummary:
 
     __name__ = 'project.work.summary'
     __metaclass__ = PoolMeta
+
 
     @classmethod
     def union_models(cls):
