77a89b1b49d6e8555810622fc953b11feaf03584
?ngel ?lvarez <angel@nan-tic.com>
2017-01-12 15:57:28 +0100

    adapt to project_management api

--- work.py
+++ work.py
@@ -1,10 +1,11 @@
+
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import PoolMeta
 from trytond.model import fields
 from trytond.pyson import Eval
 from decimal import Decimal
-
+from trytond.transaction import Transaction
 __all__ = ['ShipmentWork', 'ProjectSummary', 'Work']
 
 
@@ -17,20 +18,34 @@ class ShipmentWork:
     @classmethod
     def pending(cls, shipments):
         for shipment in shipments:
-            shipment.parent = shipment.origin
+            shipment.work_project = shipment.origin
 
         cls.save(shipments)
         super(ShipmentWork, cls).pending(shipments)
 
     @classmethod
-    def _get_cost(cls, shipments):
-        vals = cls.get_cost(shipments, 'cost')
-        return vals['cost']
+    def get_total(cls, shipments, names):
+        limit_date = Transaction().context.get('limit_date')
+        res = {}
+        for name in Work._get_summary_fields():
+            res[name] = {}
 
-    @classmethod
-    def _get_revenue(cls, shipments):
-        vals = cls.get_cost(shipments, 'revenue')
-        return vals['revenue']
+        # Calc teorical cost and revenue
+        # TODO: return values from generated sale.
+        res.update(cls.get_cost(shipments, ['cost', 'revenue']))
+
+        # Calc progress_shipments
+        progress_shipments = []
+        for shipment in shipments:
+            if limit_date != None and shipment.done_date > limit_date:
+                continue
+            progress_shipments.append(shipment)
+
+        res_p = cls.get_cost(shipments, ['cost', 'revenue'])
+        res['progress_revenue'] = res_p['revenue']
+        res['progress_cost'] = res_p['cost']
+
+        return res
 
     @staticmethod
     def _get_summary_related_field():
@@ -43,17 +58,15 @@ class Work:
     __metaclass__ = PoolMeta
 
     @classmethod
-    def _get_related_cost_and_revenue(cls):
-        res = super(Work, cls)._get_related_cost_and_revenue()
-        return res + [('shipment.work', 'project', '_get_revenue',
-            '_get_cost')]
+    def _get_summary_models(cls):
+        res = super(Work, cls)._get_summary_models()
+        return res + [('shipment.work', 'project', 'get_total')]
 
 
 class ProjectSummary:
 
     __name__ = 'project.work.summary'
     __metaclass__ = PoolMeta
-
 
     @classmethod
     def union_models(cls):
